668792b7c1529d18cea8f348e7c407c166792281
Viktor-Pakhomov97 <76701396+Viktor-Pakhomov97@users.noreply.github.com>
2021-04-21 10:56:15 +0300

    Home work 7(2)

--- src/main/java/com/pakhomovHW7/Collection.java
+++ src/main/java/com/pakhomovHW7/Collection.java
@@ -1,43 +1,101 @@
 package com.pakhomovHW7;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.util.Arrays;
 
 public class Collection {
+    private String[] mas = {"1", "2", "3"};
 
-    List<String> stringList = new ArrayList<>();
-
-    public boolean addIndexValue(int index, String element) {
-        stringList.add(index, element);
-        System.out.println(stringList);
+    boolean addIndex(String element, int indexElem) {
+        mas = Arrays.copyOf(mas, mas.length + 1);
+        System.arraycopy(mas, indexElem, mas, indexElem + 1, mas.length - indexElem - 1);
+        mas[indexElem] = element;
+        System.out.println(Arrays.toString(mas));
         return true;
     }
 
-    public boolean addValue(String element) {
-        stringList.add(element);
-        System.out.println(stringList);
+    boolean addValue(String element) {
+        mas = Arrays.copyOf(mas, mas.length + 1);
+        int indexExp = mas.length;
+        mas[indexExp - 1] = element;
+        System.out.println(Arrays.toString(mas));
         return true;
     }
 
-    public boolean removeIndex(int index) {
-        stringList.remove(index);
-        System.out.println(stringList);
-        System.out.println("Размер коллекции: " + stringList.size() + " слотов");
-        return true;
-    }
+    boolean removeIndex(int index) {
+        if (index == 0) {
+            String[] array2 = new String[mas.length - 1];
+            System.arraycopy(mas, 1, array2, 0, array2.length);
+            mas = array2;
+        } else if (index == mas.length - 1) {
+            String[] array2 = new String[mas.length - 1];
+            for (int i = 0; i < mas.length - 1; i++) {
+                array2[i] = mas[i];
+            }
+            mas = array2;
+        } else {
+            String[] array2 = new String[mas.length - 1];
+            for (int i = 0; i < mas.length; i++) {
+                if (i > index) {
+                    array2[i - 1] = mas[i];
+                }
+                if (i == index) {
+                    continue;
+                }
+                if (i < index) {
+                    array2[i] = mas[i];
+                }
+            }
+            mas = array2;
 
-    public boolean removeValue(String element) {
-        for (int i = 0; i < stringList.size(); i++) {
-            stringList.remove(element);
         }
-        System.out.println(stringList);
-        System.out.println("Размер коллекции: " + stringList.size() + " слотов");
+        System.out.println(Arrays.toString(mas));
+        return true;
+    }
+
+    boolean removeElement(String element) {
+        int index = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if (element.equals(mas[i])) {
+                break;
+            } else {
+                index++;
+            }
+        }
+        if (index == 0) {
+            String[] array2 = new String[mas.length - 1];
+            System.arraycopy(mas, 1, array2, 0, array2.length);
+            mas = array2;
+        } else if (index == mas.length - 1) {
+            String[] array2 = new String[mas.length - 1];
+            for (int i = 0; i < mas.length - 1; i++) {
+                array2[i] = mas[i];
+            }
+            mas = array2;
+        } else {
+            String[] array2 = new String[mas.length - 1];
+            for (int i = 0; i < mas.length; i++) {
+                if (i > index) {
+                    array2[i - 1] = mas[i];
+                }
+                if (i == index) {
+                    continue;
+                }
+                if (i < index) {
+                    array2[i] = mas[i];
+                }
+            }
+            mas = array2;
+
+        }
+        System.out.println(Arrays.toString(mas));
         return true;
     }
 
     public boolean printIdexList(int index) {
-        System.out.println(stringList.get(index));
+        System.out.println(mas[index]);
         return true;
     }
+
 }
+
+
--- src/main/java/com/pakhomovHW7/TestCollection.java
+++ src/main/java/com/pakhomovHW7/TestCollection.java
@@ -1,18 +1,17 @@
 package com.pakhomovHW7;
 
 
-
 public class TestCollection {
     public static void main(String[] args) {
         Collection c1 = new Collection();
-        c1.addIndexValue(0,"1");
-        c1.addIndexValue(1,"2");
-        c1.addIndexValue(2,"3");
-        c1.addIndexValue(3,"4");
-        c1.addIndexValue(4,"5");
+        c1.addIndex("4", 2);
+        c1.addIndex("5", 2);
+        c1.addValue("0");
         c1.addValue("6");
-        c1.removeIndex(1);
-        c1.removeValue("5");
-        c1.printIdexList(2);
+        c1.removeIndex(2);
+        c1.removeElement("1");
+        c1.printIdexList(0);
+
+
     }
 }
